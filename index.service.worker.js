1daaa423c10ae3cef5f7ba1056728dd529d44581
Trevor <kirchner.trevor@gmail.com>
2025-08-20 18:28:43 -0500

    Switched to godot 4.5 beta5.

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1755660321|1515514228';
+const CACHE_VERSION = '1755732390|18578606';
 /** @type {string} */
 const CACHE_PREFIX = 'Voidwood-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
