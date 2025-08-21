3150611954916a4fd59c7452513a4e5821a63c2f
Trevor <kirchner.trevor@gmail.com>
2025-08-20 21:54:47 -0500

    Level scroll reveal, minor fixes, planet 2 texture.

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1755732390|18578606';
+const CACHE_VERSION = '1755744815|47149220';
 /** @type {string} */
 const CACHE_PREFIX = 'Voidwood-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
