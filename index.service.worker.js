8be2403050351256e8bb41b305ba2bb108bf6297
Trevor <kirchner.trevor@gmail.com>
2026-05-13 18:54:29 -0500

    [release] Updated game to most recent build.

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1756003390|20417969';
+const CACHE_VERSION = '1778715830|112838890';
 /** @type {string} */
 const CACHE_PREFIX = 'Voidwood-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
