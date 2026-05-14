4186fbc92d1102b164d0c129f4d3aac99db28b26
Trevor <kirchner.trevor@gmail.com>
2026-05-13 22:26:39 -0500

    Bump cache

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1778728599|12881727448';
+const CACHE_VERSION = '1778729089|13371217418';
 /** @type {string} */
 const CACHE_PREFIX = 'Voidwood-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
