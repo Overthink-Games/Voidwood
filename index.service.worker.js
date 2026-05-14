037fe92bfaa697f0edcf3b90c277d2db95415489
Trevor <kirchner.trevor@gmail.com>
2026-05-13 22:17:22 -0500

    Fixed root position and ice slide volume.

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1778728051|12333647698';
+const CACHE_VERSION = '1778728599|12881727448';
 /** @type {string} */
 const CACHE_PREFIX = 'Voidwood-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
